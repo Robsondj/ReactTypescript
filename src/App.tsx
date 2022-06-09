68aa3054aa2de5ee4eba165bdf0866028e93ce98
Robsondj <robson.djsilva@gmail.com>
2022-06-09 14:23:16 -0300

    🚧 Added components to the main page to complete the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import List from './components/List';
+import style from "./App.module.scss";
+import Form from './components/Form';
+import { ITask } from './types/task';
+import Timer from './components/Timer';
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [selected, setSelected] = useState<ITask>();
+
+  function choseTask (selectedTask: ITask) {
+    setSelected(selectedTask);
+    setTasks( oldTasks => oldTasks.map(task => ({
+      ...task,
+      selected: task.id === selectedTask.id ? true : false
+    })));
+  }
+
+  const finishTask = () => {
+    if(selected) {
+      setSelected(undefined);
+      setTasks( oldTasks => oldTasks.map(task => {
+        if(task.id === selected.id) {
+          return {
+            ...task,
+            selected: false,
+            completed: true
+          }
+        }
+        return task;
+      }));
+    }
+  }
+
   return (
-    <div>
+    <div className={style.AppStyle}>
+      <Form setTasks={setTasks} />
+      <List choseTask={choseTask} tasks={tasks} />
+      <Timer selected={selected} finishTask={finishTask} />
     </div>
   );
 }
